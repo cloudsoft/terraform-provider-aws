571a840a2dd304551e236162f1894aef3100162f
Paul Campbell <pcampbell@kemitix.net>
2020-06-17 15:52:44 +0100

    sc-constraint: add read implementation

--- aws/resource_aws_servicecatalog_launch_role_constraint.go
+++ aws/resource_aws_servicecatalog_launch_role_constraint.go
@@ -3,9 +3,11 @@ package aws
 import (
 	"fmt"
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/service/servicecatalog"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+	"log"
 	"regexp"
 	"time"
 )
@@ -60,6 +62,14 @@ func resourceAwsServiceCatalogConstraint() *schema.Resource {
 					"TEMPLATE"},
 					false),
 			},
+			"owner": {
+				Type: schema.TypeString,
+				Computed: true,
+			},
+			"status": {
+				Type: schema.TypeString,
+				Computed: true,
+			},
 		},
 	}
 }
@@ -84,6 +94,27 @@ func createConstraint(d *schema.ResourceData, meta interface{}) error {
 }
 
 func readConstraint(d *schema.ResourceData, meta interface{}) error {
+	conn := meta.(*AWSClient).scconn
+	input := servicecatalog.DescribeConstraintInput{
+		Id: aws.String(d.Id()),
+	}
+	constraint, err := conn.DescribeConstraint(&input)
+	if err != nil {
+		if scErr, ok := err.(awserr.Error); ok && scErr.Code() == servicecatalog.ErrCodeResourceNotFoundException {
+			log.Printf("[WARN] Service Catalog Constraint %s not found, removing from state", d.Id())
+			d.SetId("")
+			return nil
+		}
+		return fmt.Errorf("reading Service Catalog Constraint '%s' failed: %s", *input.Id, err.Error())
+	}
+	details := constraint.ConstraintDetail
+	d.Set("description", details.Description)
+	d.Set("portfolio_id", details.PortfolioId)
+	d.Set("product_id", details.ProductId)
+	d.Set("parameters", constraint.ConstraintParameters)
+	d.Set("type", details.Type)
+	d.Set("owner", details.Owner)
+	d.Set("status", constraint.Status)
 	return nil
 }
 
